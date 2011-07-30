9ce4806953ce74b1e95fb35e78f33202c7c2cfab
Joshua Piccari <joshua.piccari@gmail.com>
2011-07-29 23:55:07 -0700

    Initial Commit

---
+++ FScriptEverywhere/FScriptEverywhereAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  FScriptEverywhereAppDelegate.h
+//  FScriptEverywhere
+//
+//  Created by Joshua Piccari on 7/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FScriptEverywhereAppDelegate : NSObject <NSApplicationDelegate> {
+	NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FScriptEverywhere/FScriptEverywhereAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  FScriptEverywhereAppDelegate.m
+//  FScriptEverywhere
+//
+//  Created by Joshua Piccari on 7/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FScriptEverywhereAppDelegate.h"
+
+@implementation FScriptEverywhereAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
